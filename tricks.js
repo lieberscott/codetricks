6dd31892f7b9fa0c2ba7e5964831387c4ee40775
Scott Lieber <WSox1235@aol.com>
2018-07-25 14:55:12 -0400

    Convert array into object's key values

---
+++ tricks.js
@@ -0,0 +1,23 @@
+// 1. Convert an array into an object's key values
+
+let arr = ['Boston Celtics', 'Golden State Warriors', 'Houston Rockets', 'Los Angeles Lakers', 'Toronto Raptors'];
+
+let obj = {};
+
+arr.map((val, ind) => {
+  obj[`${value}`] = 0
+  return temp_obj;
+});
+
+/* Output
+
+
+obj = {
+  Boston Celtics: 0,
+  Golden State Warriors: 0,
+  Houston Rockets: 0,
+  Los Angeles Lakers: 0,
+  Toronto Raptors: 0
+}
+
+*/
